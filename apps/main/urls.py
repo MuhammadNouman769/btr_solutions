b02e04a4479dc063965e8ddff97c56d30eabaf73
Muhammad Nouman <noumannisar769@gmail.com>
2026-06-13 16:53:29 +0500

    Refactor homepage and header layout, update styles, and add new images

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('about/', views.about, name='about_us'),
     path('contact/', views.contact, name='contact'),
     path('services/', views.services, name='services'),
     path('projects/', views.projects, name='projects'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
-    path('blog-single/', views.blog_single, name='blog-single'),
+    path('blog-single/', views.blog_single, name='blog_single'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
-    path('about/', include('apps.main.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
